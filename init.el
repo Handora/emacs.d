f1c71ce82dc937fabf0064a378c51b0034e3d01b
Handora <qcdsr970209@gmail.com>
2018-02-15 16:41:10 +0800

    add python support

--- init.el
+++ init.el
@@ -11,3 +11,17 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
