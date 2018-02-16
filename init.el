d146143946bca806f5804583015218f6c4989862
Handora <qcdsr970209@gmail.com>
2018-02-16 18:25:41 +0800

    add idet, web mode, emacsclient

--- init.el
+++ init.el
@@ -18,10 +18,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (aggressive-indent expand-region hungry-delete beacon flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
+    (better-shell web-mode iedit aggressive-indent expand-region hungry-delete beacon flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
