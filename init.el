212259f3a4b0e256e8f2e30ea8b4343c948379d2
Handora <qcdsr970209@gmail.com>
2018-02-19 15:41:39 +0800

    addd nyan and magit.

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
-(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 
 (package-initialize)
 
@@ -18,7 +18,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (treemacs-projectile treemacs emmet-mode dumb-jump counsel-projectile projectile smartparens ggtags better-shell web-mode iedit aggressive-indent expand-region hungry-delete beacon flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
+    (magit nyan-mode ivy-hydra markdown-mode treemacs-projectile treemacs emmet-mode dumb-jump counsel-projectile projectile smartparens ggtags better-shell web-mode iedit aggressive-indent expand-region hungry-delete beacon flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
