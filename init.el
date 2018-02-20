8fed60caddcf0a62fc5af07328c6b2ee20122534
Handora <qcdsr970209@gmail.com>
2018-02-21 00:29:43 +0800

    Some bug with gcal, need be fixed

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")
+			 ("org" . "http://elpa.emacs-china.org/org/")))
 
 (package-initialize)
 
@@ -17,9 +18,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (flycheck-gometalinter go-errcheck go-eldoc company-go go-mode company-jedi rainbow-delimiters bm magit nyan-mode ivy-hydra markdown-mode treemacs-projectile treemacs emmet-mode dumb-jump counsel-projectile projectile smartparens ggtags better-shell web-mode iedit aggressive-indent expand-region hungry-delete beacon flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
+    (calfw-ical calfw-org calfw-gcal calfw org-gcal org-bullets flycheck-gometalinter go-errcheck go-eldoc company-go go-mode company-jedi rainbow-delimiters bm magit nyan-mode ivy-hydra markdown-mode treemacs-projectile treemacs emmet-mode dumb-jump counsel-projectile projectile smartparens ggtags better-shell web-mode iedit aggressive-indent expand-region hungry-delete beacon flycheck elfeed-org borg zenburn-theme which-key use-package try counsel color-theme auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
